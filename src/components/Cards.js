79f6c40f906d4e831599d6690235dd996a212bd0
Leonardo <leoigeltiko15@gmail.com>
2020-06-24 17:19:54 +0300

    div was removed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -48,51 +48,49 @@ const Cards = ({ data }) => {
       {newData.current &&
         newData.current.map((card, index) => {
           return (
-            <div key={card.id}>
-              <CardsContainer width={width}>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <Info onClick={() => addFavorites(card)}>
-                    <MoreHorizIcon />
-                  </Info>
-                  {id.id === card.id && id.open ? (
-                    <BoxFavorite card={card} />
-                  ) : null}
-                </div>
-                <ImgContainer onClick={() => addCardToFavorite(card)}>
-                  {card.backdrop_path ? (
-                    <img
-                      style={{
-                        display: 'block',
-                        height: '274px',
-                        width: '20px',
-                        minWidth: '100%',
-                        borderRadius: '10px 10px 0px 0px',
-                        objectFit: 'cover',
-                      }}
-                      src={`${`https://image.tmdb.org/t/p/original/${card.backdrop_path}`}`}
-                      alt="card-img"
-                    />
-                  ) : (
-                    <ImgNotFound />
-                  )}
-                  <HeartIcon>
-                    <i className="fas fa-thumbs-up"></i>
-                  </HeartIcon>
-                </ImgContainer>
-                <ContainerInformation>
-                  <ContainerDetails>
-                    <VoteAverage>
-                      {card.vote_average}
-                      <Percent>%</Percent>
-                    </VoteAverage>
-                    <Div>
-                      <Date>{`${moment(card.release_date).format('LL')}`}</Date>
-                      <Title>{card.title}</Title>
-                    </Div>
-                  </ContainerDetails>
-                </ContainerInformation>
-              </CardsContainer>
-            </div>
+            <CardsContainer width={width}>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Info onClick={() => addFavorites(card)}>
+                  <MoreHorizIcon />
+                </Info>
+                {id.id === card.id && id.open ? (
+                  <BoxFavorite card={card} />
+                ) : null}
+              </div>
+              <ImgContainer onClick={() => addCardToFavorite(card)}>
+                {card.backdrop_path ? (
+                  <img
+                    style={{
+                      display: 'block',
+                      height: '274px',
+                      width: '20px',
+                      minWidth: '100%',
+                      borderRadius: '10px 10px 0px 0px',
+                      objectFit: 'cover',
+                    }}
+                    src={`${`https://image.tmdb.org/t/p/original/${card.backdrop_path}`}`}
+                    alt="card-img"
+                  />
+                ) : (
+                  <ImgNotFound />
+                )}
+                <HeartIcon>
+                  <i className="fas fa-thumbs-up"></i>
+                </HeartIcon>
+              </ImgContainer>
+              <ContainerInformation>
+                <ContainerDetails>
+                  <VoteAverage>
+                    {card.vote_average}
+                    <Percent>%</Percent>
+                  </VoteAverage>
+                  <Div>
+                    <Date>{`${moment(card.release_date).format('LL')}`}</Date>
+                    <Title>{card.title}</Title>
+                  </Div>
+                </ContainerDetails>
+              </ContainerInformation>
+            </CardsContainer>
           );
         })}
     </Container>
